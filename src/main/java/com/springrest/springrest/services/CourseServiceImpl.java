50bf05a89152eb7d77c90088ec54ccc77acc1ed0
Training <malakar.abh@gmail.com>
2022-01-27 08:12:51 +0000

    first commit

--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -1,30 +1,31 @@
 package com.springrest.springrest.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService{
 
-	List<Course> list;
+	@Autowired
+	private CourseDao courseDao;
+	//List<Course> list;
 	
 	public CourseServiceImpl() {
 		// TODO Auto-generated constructor stub
 		
-		list=new ArrayList<>();
-		list.add(new Course(145,"Java Core Course","This is java basic course"));
-		list.add(new Course(146,"Python Course","This is Python basic course"));
+		//list=new ArrayList<>();
+		//list.add(new Course(145,"Java Core Course","This is java basic course"));
+		//list.add(new Course(146,"Python Course","This is Python basic course"));
 	}
 	
 	
 	public List<Course> getCourses() {
 		// TODO Auto-generated method stub
-		return list;
+		return courseDao.findAll();
 	}
 
 
@@ -32,15 +33,15 @@ public class CourseServiceImpl implements CourseService{
 	public Course getCourse(long courseId) {
 		// TODO Auto-generated method stub
 		
-		Course c=null;
+		/*Course c=null;
 		
 		for(Course course:list){
 			if(course.getId()==courseId) {
 				c=course;
 				break;
 			}
-		}
-		return c;
+		}*/
+		return courseDao.getOne(courseId);
 	}
 
 
@@ -48,7 +49,9 @@ public class CourseServiceImpl implements CourseService{
 	public Course addCourse(Course course) {
 		// TODO Auto-generated method stub
 		
-		list.add(course);
+		//list.add(course);
+		
+		courseDao.save(course);
 		return course;
 	}
 
@@ -57,13 +60,14 @@ public class CourseServiceImpl implements CourseService{
 	public Course updateCourse(Course course) {
 		// TODO Auto-generated method stub
 		
-		list.forEach(e -> {
+	/*	list.forEach(e -> {
 			if(e.getId() == course.getId()) {
 				e.setTitle(course.getTitle());
 				e.setDescription(course.getDescription());
 			}
-		});
+		});*/
 		
+		courseDao.save(course);
 		
 		return course;
 	}
@@ -73,8 +77,9 @@ public class CourseServiceImpl implements CourseService{
 	public void deleteCourse(long parseLong) {
 		// TODO Auto-generated method stub
 		
-		list=this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
-		
+		//list=this.list.stream().filter(e->e.getId()!=parseLong).collect(Collectors.toList());
+		Course entity=courseDao.getOne(parseLong);
+		courseDao.delete(entity);
 	}
 
 }
